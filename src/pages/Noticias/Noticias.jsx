0ffad29c20f29ba9f6fbe80b910114e824ea801f
HenAi <henai.hh@gmail.com>
2026-04-23 14:42:27 +0000

    style: replace emoji with lucide square-arrow-out-up-right SVG icon

--- src/pages/Noticias/Noticias.jsx
+++ src/pages/Noticias/Noticias.jsx
@@ -106,10 +106,25 @@ function Noticias() {
                             color: 'inherit',
                             display: 'inline-flex',
                             alignItems: 'center',
-                            gap: '5px',
+                            gap: '6px',
                           }}
                         >
-                          🔗 Ver la noticia
+                          Ver la noticia
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="14"
+                            height="14"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          >
+                            <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+                            <path d="m21 3-9 9" />
+                            <path d="M15 3h6v6" />
+                          </svg>
                         </a>
                       </div>
                     )}
